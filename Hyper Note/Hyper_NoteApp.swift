1947e03551216305c0627e45bc71e780aaf7b50d
xiikii <andy864940012@gmail.com>
2024-11-09 00:07:45 +0800

    Initial Commit

---
+++ Hyper Note/Hyper_NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hyper_NoteApp.swift
+//  Hyper Note
+//
+//  Created by LC John on 2024/11/9.
+//
+
+import SwiftUI
+
+@main
+struct Hyper_NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hyper NoteTests/Hyper_NoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  Hyper_NoteTests.swift
+//  Hyper NoteTests
+//
+//  Created by LC John on 2024/11/9.
+//
+
+import Testing
+@testable import Hyper_Note
+
+struct Hyper_NoteTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
