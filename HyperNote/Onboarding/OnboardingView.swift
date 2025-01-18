62b465609a9bb5e77036a44a90b8785e232560e1
xiikii <andy864940012@gmail.com>
2025-01-18 22:50:40 +0800

    Add onboarding view and window controller for user introduction

---
+++ HyperNote/Onboarding/OnboardingView.swift
@@ -0,0 +1,8 @@
+//
+//  OnboardingView.swift
+//  HyperNote
+//
+//  Created by LC John on 1/17/25.
+//
+
+import Foundation
---
+++ HyperNote/Onboarding/OnboardingWindowController.swift
@@ -0,0 +1,8 @@
+//
+//  OnboardingWindowController.swift
+//  HyperNote
+//
+//  Created by LC John on 1/17/25.
+//
+
+import Foundation
